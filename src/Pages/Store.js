025a76e3ebfc3b38a9bfad800fa73eb70ba9aa69
Hudson Bean <hudsonbeandev@gmail.com>
2024-06-29 09:40:52 -0500

    Setup react router dom

--- src/Pages/Store.js
+++ src/Pages/Store.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Store() {
-  return <div>Store</div>;
+  return <div>Hello World!</div>;
 }
 
 export default Store;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Store from "./Store";
+import Home from "./Pages/Home";
+import Store from "./Pages/Store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={Home}></Route>
-      <Route path="/Store" element={Store}></Route>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/Store" element={<Store />}></Route>
     </Routes>
   </BrowserRouter>
 );
