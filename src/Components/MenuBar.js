302e72fed0a19ffb97cf520d58a83dd82a5041d2
Hudson Bean <hudsonbeandev@gmail.com>
2024-06-29 13:37:47 -0500

    Setting up menu-bar

--- src/Components/MenuBar.js
+++ src/Components/MenuBar.js
@@ -1,8 +1,22 @@
-import React from "react";
+// Styles
 import "../Styles/Components/MenuBar.css";
+// Imports
+import React from "react";
+import Button from "./Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import classNames from "classnames";
 
 function MenuBar() {
-  return <div className="menu-bar">MenuBar</div>;
+  return (
+    <div className="menu-bar">
+      <Button className="menu-bar__item">
+        <FontAwesomeIcon icon={faBars} />
+      </Button>
+      <ul className={classNames("menu-bar__item", "menu-bar__list")}></ul>
+      <Button className="menu-bar__item">Shop</Button>
+    </div>
+  );
 }
 
 export default MenuBar;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Store from "./Pages/Store";
