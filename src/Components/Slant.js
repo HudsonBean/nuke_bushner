3575f39b375e73cf93de470c59fd342261a25bd2
Hudson Bean <hudsonbeandev@gmail.com>
2024-06-30 00:15:19 -0500

    V4

--- src/Components/Slant.js
+++ src/Components/Slant.js
@@ -3,12 +3,12 @@ import React from "react";
 function Slant({ className }) {
   return (
     <svg
-      style={{ position: "absolute", bottom: 0, width: "100%", height: "5vw" }}
+      style={{ position: "absolute", bottom: 0, width: "100%", height: "10vw" }}
       className={className}
       viewBox="0 0 100 100"
       preserveAspectRatio="none"
     >
-      <polygon points="0, 100 100, 0 100 100" />
+      <polygon points="0, 0 0, 100 100 100" />
     </svg>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,15 +2,17 @@ import React from "react";
 import "../Styles/Home.css";
 import MenuBar from "../Components/MenuBar";
 import Slant from "../Components/Slant";
+import classNames from "classnames";
 
 function Home() {
   return (
     <div>
       <div className="home-page">
         <MenuBar />
-        <Slant className="slant" />
+        <Slant className={classNames("slant", "grit")} />
       </div>
       <div
+        className="grit"
         style={{ width: "100%", height: "50rem", backgroundColor: "white" }}
       ></div>
     </div>
