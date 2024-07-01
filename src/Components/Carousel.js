902d6a9dbb6b1e43553c62296a71536fff5d215e
Hudson Bean <hudsonbeandev@gmail.com>
2024-07-01 18:46:10 -0500

    Fade-in for list done

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Styles/Components/Carousel.css";
 import Button from "./Button";
+import { useMotionValueEvent, useScroll } from "framer-motion";
+import classNames from "classnames";
 
 function Carousel() {
+  const { scrollY } = useScroll();
+  const [viewable, setViewable] = useState("viewable--false");
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    if (latest >= 430) {
+      setViewable("viewable--true");
+    } else if (latest >= 850) {
+      setViewable("viewable--false");
+    } else {
+      setViewable("viewable--false");
+    }
+  });
   const data = [
     {
       Date: "July 6, 2024",
@@ -29,7 +42,7 @@ function Carousel() {
     <div className="carousel-container">
       <div className="carousel">
         {data.map((item, i) => (
-          <div className="slide" key={i}>
+          <div className={classNames("slide", viewable)} key={i}>
             <span className="slide__date">{item.Date}</span>
             <span className="slide__area">{item.Area}</span>
             <span className="slide__location">
