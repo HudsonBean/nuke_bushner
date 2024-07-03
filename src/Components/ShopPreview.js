54ec6a66ac805b60bbfe81633361954c92e99f24
Hudson Bean <hudsonbeandev@gmail.com>
2024-07-02 19:24:16 -0500

    Finished shop-preview

--- src/Components/ShopPreview.js
+++ src/Components/ShopPreview.js
@@ -1,15 +1,35 @@
 import { useMotionValueEvent, useScroll } from "framer-motion";
 import React, { useState } from "react";
 import SlantLeft from "./SlantLeft";
+import shirt from "../Images/ShirtPlaceholder.png";
+import Button from "./Button";
 
 function ShopPreview() {
   const { scrollY } = useScroll();
   const [shopTitleX, setShopTitleX] = useState(0);
+  const [isViewable, setViewable] = useState({
+    opacity: "1",
+    transition: "all 0.65s ease-in-out",
+    transform: "translateX(0px)",
+  });
   useMotionValueEvent(scrollY, "change", (latest) => {
     if (latest >= 960) {
       setShopTitleX(latest - 960);
     } else {
       setShopTitleX(0);
+    }
+    if (latest >= 1150) {
+      setViewable({
+        opacity: "1",
+        transition: "all 0.65s ease-in-out",
+        transform: "translateX(0px)",
+      });
+    } else {
+      setViewable({
+        opacity: "0",
+        transition: "all 0.65s ease-in-out",
+        transform: "translateX(-20px)",
+      });
     }
   });
   return (
@@ -22,6 +42,17 @@ function ShopPreview() {
       >
         Shop
       </span>
+      <div className="shop-preview__content-wrapper">
+        <img
+          alt="Shirts from Nuke Bushner's store."
+          src={shirt}
+          className="shop-preview__image"
+          style={isViewable}
+        />
+        <Button newPage={true} href="/store" className="shop-preview__button">
+          Shop
+        </Button>
+      </div>
       <SlantLeft right={false} className="slant" />
     </div>
   );
